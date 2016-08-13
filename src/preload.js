3dca4e5717057753dbf354b2f225fa7e9ec51079
Adán Sánchez de Pedro Crespo <adan@stampery.com>
2016-08-13 14:20:17 +0200

    Expose eRequire

--- src/preload.js
+++ src/preload.js
@@ -1,6 +1,1 @@
-electron = require('electron').remote;
-fs = require('fs');
-git = require('simple-git');
-mkdir = require('mkdirp');
-request = require('request');
-os = require('os');
+eRequire = require;
